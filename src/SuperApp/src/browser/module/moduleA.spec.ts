3d8ad3a2ee874c7cda2c0ae8c6b17cd3851214cd
Sergey Nikitin <sen@tradeshift.com>
2018-09-26 19:35:29 +0700

    Initial commit

---
+++ src/SuperApp/src/browser/module/moduleA.spec.ts
@@ -0,0 +1,9 @@
+import {ModuleA} from './moduleA'
+
+describe('CompanySearch Logic: CompanySearch Class', () => {
+  it('', () => {
+    const g = new ModuleA();
+    expect(g.sum(1, 2)).toBe(3);
+  })
+});
+
---
+++ src/SuperApp/src/browser/module/moduleA.ts
@@ -0,0 +1,5 @@
+export class ModuleA {
+    public sum(a: number, b: number): number {
+        return a + b;
+    }
+}
